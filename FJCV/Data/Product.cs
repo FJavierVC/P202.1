eb3726e5f99fd65bdcd590f4aaa237d174cb8744
Francisco Javier Valdez Cuellar <iti4469@umad.edu.mx>
2023-03-06 09:18:42 -0600

    Creacion de clasesSe agregan clases Product y Provider

---
+++ FJCV/Data/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace FJCV.Data
+{
+    public class Product
+    {
+        #region Propiedades:
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string SerialNumber { get; set; }
+        public float Price { get; set; }
+
+        #endregion
+    }
+}
